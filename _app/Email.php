b3179e4457478be66ee2c402e3873efb45b9199b
Stefanovicz <rafael.stefanovicz@gmail.com>
2018-09-03 11:40:35 -0300

    Commit Initial

---
+++ _app/Email.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Notification;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Email {
+
+    private $mail = \stdClass::class;
+
+    public function __construct() {
+        $mail = new PHPMailer(true);
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'user@example.com';                 // SMTP username
+        $mail->Password = 'secret';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;
+    }
+
+    public function sendMail() {
+        echo "E-mail enviado!";
+    }
+
+}
